69a214b2d7fce8ca62829c7801fc9fcd63848423
sunil Chauhan <sunilkms@gmail.com>
2021-05-16 14:24:12 +0530

    Update Start_Vaccine_center_Monitor.bat

--- Start_Vaccine_center_Monitor.bat
+++ Start_Vaccine_center_Monitor.bat
@@ -1,2 +1,2 @@
 echo 0 > stop.cfg
-powershell.exe -WindowStyle hidden -NoProfile -File "Find_Covid_Vaccine_Centers.ps1"
+powershell.exe -WindowStyle hidden -ExecutionPolicy Bypass -STA -NoProfile -File "Find_Covid_Vaccine_Centers.ps1"
